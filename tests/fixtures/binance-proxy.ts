2b4b405596c123cc1cfb060bf2d599d1ec7007de
iamseki <chrisyuri_19@hotmail.com>
2021-07-15 22:59:18 -0300

    test: order usecase

--- tests/fixtures/binance-proxy.ts
+++ tests/fixtures/binance-proxy.ts
@@ -26,12 +26,12 @@ const accountInfoResponse: AccountInfoResponse = {
   ],
 };
 
-const sellOrderResponse: NewOrderResponse = {
+const buyOrderResponse: NewOrderResponse = {
   clientOrderId: '1234',
   price: '0',
-  executedQty: '0.000951',
-  origQty: '0.000951',
-  cummulativeQuoteQty: '30.24',
+  executedQty: '0.0009140',
+  origQty: '0.0009140',
+  cummulativeQuoteQty: '29.0652',
   orderId: 1,
   orderListId: 2,
   type: 'MARKET',
@@ -43,36 +43,36 @@ const sellOrderResponse: NewOrderResponse = {
   fills: [
     {
       price: '31800.00',
-      qty: '0.00094500',
+      qty: '0.00091400',
       // qty * price * fee , fee = 0,001
-      commission: '0.030051',
-      commissionAsset: 'USDT',
+      commission: '0.00000091',
+      commissionAsset: 'BTC',
       tradeId: 7129774,
     },
   ],
 };
 
-const buyOrderResponse: NewOrderResponse = {
+const sellOrderResponse: NewOrderResponse = {
   clientOrderId: '1234',
   price: '0',
-  executedQty: '0.000951',
-  origQty: '0.000951',
-  cummulativeQuoteQty: '30.24',
+  executedQty: '0.0009140',
+  origQty: '0.0009140',
+  cummulativeQuoteQty: '29.0652',
   orderId: 1,
   orderListId: 2,
   type: 'MARKET',
   transactTime: 1626388750934,
   symbol: 'BTCUSDT',
-  side: 'BUY',
+  side: 'SELL',
   status: 'FILLED',
   timeInForce: 'GTC',
   fills: [
     {
       price: '31800.00',
-      qty: '0.00094500',
+      qty: '0.0009140',
       // qty * fee , fee = 0,001
-      commission: '0.000000945',
-      commissionAsset: 'BTC',
+      commission: '0.0290652',
+      commissionAsset: 'USDT',
       tradeId: 7129777,
     },
   ],
---
+++ tests/usecases/order.spec.ts
@@ -0,0 +1,33 @@
+import { fakeBinanceProxy } from '../fixtures/binance-proxy';
+import { Order } from '../../src/usecases';
+import { Side, Status } from '../../src/models/order';
+
+/*
+ ** the symbol and quantity its hard coded in fake proxy response so keep that in mind
+ ** the value of bitcoin is hardcoded being = $31800 aswell
+ */
+describe('order - Use Case', () => {
+  it('should execute a buy order', async () => {
+    const order = new Order(fakeBinanceProxy);
+    const result = await order.buy('BTCUSDT', 0.000914);
+
+    expect(result.status).toBe(Status.FILLED);
+    expect(result.side).toBe(Side.BUY);
+    expect(result.executedQuantity).toBe('0.0009140');
+    expect(result.currency).toBe('29.0652');
+    expect(result.fills[0].exchangeComission).toBe('0.00000091');
+    expect(result.fills[0].symbol).toBe('BTC');
+  });
+
+  it('should execute a sell order', async () => {
+    const order = new Order(fakeBinanceProxy);
+    const result = await order.sell('BTCUSDT', 0.000914);
+
+    expect(result.status).toBe(Status.FILLED);
+    expect(result.side).toBe(Side.SELL);
+    expect(result.executedQuantity).toBe('0.0009140');
+    expect(result.currency).toBe('29.0652');
+    expect(result.fills[0].exchangeComission).toBe('0.0290652');
+    expect(result.fills[0].symbol).toBe('USDT');
+  });
+});
